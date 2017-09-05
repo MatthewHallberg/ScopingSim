879caadc73fc5ff33cdf266cae301e0f25b29882
matthew hallberg <matthewhallberg@gmail.com>
2017-09-05 01:09:03 -0400

    redid fade to black, added scene service.

---
+++ Assets/Scripts/BlackFadeBehavior.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlackFadeBehavior : MonoBehaviour {
+
+	private static BlackFadeBehavior _instance;
+
+	public static BlackFadeBehavior Instance {
+		get {
+			return _instance;
+		}
+	}
+
+	private CanvasGroup cv;
+	private float desiredAlpha;
+	private bool shouldFade = false;
+
+	void Awake(){
+		_instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+		cv = GetComponent<CanvasGroup> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (shouldFade) {
+			cv.alpha = Mathf.Lerp (cv.alpha, desiredAlpha, Time.deltaTime * 6f);
+		}
+	}
+
+	public void FadeToBlack(){
+		StartCoroutine (Fade ());
+	}
+
+	IEnumerator Fade(){
+		desiredAlpha = 1f;
+		shouldFade = true;
+		yield return new WaitForSeconds (1f);
+		desiredAlpha = 0f;
+		yield return new WaitForSeconds (1f);
+		shouldFade = false;
+	}
+}
---
+++ Assets/Scripts/SceneService.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneService : MonoBehaviour {
+
+	public static SceneService instance;
+
+	void Awake() {
+		if(instance == null)  {
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+		} else if(instance != this) {
+			Destroy(gameObject);
+		}
+	}
+
+	public void LoadScene(int sceneIndex){
+		StartCoroutine (LoadSceneRoutine (sceneIndex));
+	}
+
+	IEnumerator LoadSceneRoutine(int sceneIndex){
+		BlackFadeBehavior.Instance.FadeToBlack ();
+		yield return new WaitForSeconds (1f);
+		SceneManager.LoadScene (sceneIndex);
+	}
+}
