a7e12b2bd14ccd51f5b50a5b8f7cf2bdcef0fa6d
matthew hallberg <matthewhallberg@gmail.com>
2017-11-11 21:42:27 -0500

    Blood flow second pass, covering whole camera view.

---
+++ Assets/Scripts/BloodFlowBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodFlowBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void OnEnable () {
+		StartCoroutine (BloodFlowRoutine ());
+	}
+
+	IEnumerator BloodFlowRoutine(){
+
+		//first turn on blood
+		foreach (Transform child in this.transform) {
+			ParticleSystem ps = child.GetComponent<ParticleSystem> ();
+			var main = ps.main;
+			main.simulationSpeed = 1000f;
+		}
+
+		yield return new WaitForEndOfFrame ();
+
+		//now slow simulation speed
+		foreach (Transform child in this.transform) {
+			ParticleSystem ps = child.GetComponent<ParticleSystem> ();
+			var main = ps.main;
+			main.simulationSpeed = .1f;
+		}
+	}
+}
